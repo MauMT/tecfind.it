ea9c4d9b02dacd7c5639f8dfbf55dedc1a629e5c
Mauricio Martinez Toledo <mauriciomtzt@hotmail.com>
2021-11-22 02:16:04 -0600

    changed to mongodb atlas

--- src/database.js
+++ src/database.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config({ path: path.resolve(__dirname, './.env') });
-
+/*
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('connection successful'))
     .catch((err) => console.error(err));
+*/
+
+mongoose.connect(process.env.MONGODB_ATLAS)
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.error(err));
+
+
+
+
+
 
 module.exports = mongoose;
